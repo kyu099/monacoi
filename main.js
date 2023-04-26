eae019cde707ce39881a954f17161218a2b0bd68
kyu099 <ryu_gelus1277@icloud.com>
2023-04-26 18:03:04 +0900

    Update main.js
    配置変更

--- main.js
+++ main.js
@@ -36,22 +36,22 @@ function draw(ctx) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "yellow";
     ctx.fillRect(0, 0, 180, 240);
-    ctx.fillRect(canvas.width - 180, 0, 180, 240);
     ctx.fillRect(0, 250, 180, 240);
-    ctx.fillRect(canvas.width - 180, 250, 180, 240);
+    ctx.fillRect(0, 500, 180, 240);
+    ctx.fillRect(0, 750, 180, 240);
     ctx.fillStyle = "red"
 
     if(players[0] == 1){
         drawCross(150, 120, 60, ctx);
     }
     if(players[1] == 1){
-        drawCross(canvas.width - 150, 120, 60, ctx);  
+        drawCross(150, 120 + 250, 60, ctx);  
     }
     if(players[2] == 1){
-        drawCross(150, 120 + 250, 60, ctx);
+        drawCross(150, 120 + 500, 60, ctx);
     }
     if(players[3] == 1){
-        drawCross(canvas.width - 150, 120 + 250, 60, ctx);
+        drawCross(150, 120 + 750, 60, ctx);
     }
 
     for(i = 0; i < waiting.length; i++) {
@@ -62,23 +62,23 @@ function draw(ctx) {
     }
     ctx.fillStyle = "red"
     ctx.font = '32px sans-serif';
-    ctx.fillText("空き:" + String((4 - countPlayers())/2), 10, 590);
-    ctx.fillText("待ち:" + String(waiting.length), 10, 630);
+    ctx.fillText("空き:" + String((4 - countPlayers())/2), 300, 940);
+    ctx.fillText("待ち:" + String(waiting.length), 300, 980);
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
 draw(ctx);
 
 canvas.addEventListener("click", (e) => {
-    let x = e.clientX - canvas.getBoundingClientRect().left - 240;
-    let y = e.clientY - canvas.getBoundingClientRect().top - 245;
-    if(y < 245){
-        if(x < 0 && y < 0) {players[0] = -players[0];}
-        if(x > 0 && y < 0) {players[1] = -players[1];}
-        if(x < 0 && y > 0) {players[2] = -players[2];}
-        if(x > 0 && y > 0) {players[3] = -players[3];}
+    let x = e.clientX - canvas.getBoundingClientRect().left;
+    let y = e.clientY - canvas.getBoundingClientRect().top;
+    if(x < 200){
+        if(y < 240) {players[0] = -players[0];}
+        if(y > 250 && y < 490) {players[1] = -players[1];}
+        if(y > 500 && y < 740) {players[2] = -players[2];}
+        if(y > 750 && y < 990) {players[3] = -players[3];}
     } else {
-        waiting.push({x: x + 240, y: y + 245});
+        waiting.push({x: x, y: y});
     }
     draw(ctx);
     //console.log(x, y, waiting.length);
