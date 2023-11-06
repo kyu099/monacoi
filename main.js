0f13382e566c06bc2a42f13c9c1ee697757bd45c
kyu099 <ryu_gelus1277@icloud.com>
2023-11-06 12:43:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 const ctx = canvas.getContext("2d");
 const share = document.getElementById("sharebutton");
 
-// ハロウィン仕様
-let tombstoneimg = new Image();
-tombstoneimg.src = "halloween_grave.png"
-let pumpkinimg = new Image();
-pumpkinimg.src = "halloween_pumpkin1.png"
-let ghostimg = new Image();
-ghostimg.src = "halloween_chara7_obake.png"
-
 let players = [1, 1, 1, 1];
 let waiting = [];
 
@@ -39,45 +31,33 @@ function drawCross(x, y, size, ctx) {
 }
 
 function draw(ctx) {
-    // ハロウィン仕様
-    //ctx.fillStyle = "white";
-    ctx.fillStyle = "#ba55d3"
+    ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(tombstoneimg, 0, 0, 348, 240);
-    ctx.drawImage(tombstoneimg, 0, 250, 348, 240);
-    ctx.drawImage(tombstoneimg, 0, 500, 348, 240);
-    ctx.drawImage(tombstoneimg, 0, 750, 348, 240);
-    
-    //ctx.fillStyle = "yellow";
-    //ctx.fillRect(0, 0, 180, 240);
-    //ctx.fillRect(0, 250, 180, 240);
-    //ctx.fillRect(0, 500, 180, 240);
-    //ctx.fillRect(0, 750, 180, 240);
-    //ctx.fillStyle = "red"
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(0, 0, 180, 240);
+    ctx.fillRect(0, 250, 180, 240);
+    ctx.fillRect(0, 500, 180, 240);
+    ctx.fillRect(0, 750, 180, 240);
+    ctx.fillStyle = "red"
     
     if(players[0] == 1){
-        //drawCross(150, 120, 60, ctx);
-        ctx.drawImage(ghostimg, 150, 20, 187, 200);
+        drawCross(150, 120, 60, ctx);
     }
     if(players[1] == 1){
-        //drawCross(150, 120 + 250, 60, ctx);  
-        ctx.drawImage(ghostimg, 150, 20 + 250, 187, 200);
+        drawCross(150, 120 + 250, 60, ctx);  
     }
     if(players[2] == 1){
-        //drawCross(150, 120 + 500, 60, ctx);
-        ctx.drawImage(ghostimg, 150, 20 + 500, 187, 200);
+        drawCross(150, 120 + 500, 60, ctx);
     }
     if(players[3] == 1){
-        //drawCross(150, 120 + 750, 60, ctx);
-        ctx.drawImage(ghostimg, 150, 20 + 750, 187, 200);
+        drawCross(150, 120 + 750, 60, ctx);
     }
 
     for(i = 0; i < waiting.length; i++) {
-        //ctx.fillStyle = "black";
-        //ctx.beginPath();
-        //ctx.arc(waiting[i].x, waiting[i].y, 60, 0, 360 * Math.PI / 180, false);
-        //ctx.fill();
-        ctx.drawImage(pumpkinimg, waiting[i].x - 75, waiting[i].y - 64, 150, 128);
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.arc(waiting[i].x, waiting[i].y, 60, 0, 360 * Math.PI / 180, false);
+        ctx.fill();
     }
     ctx.fillStyle = "red"
     ctx.font = '64px sans-serif';
@@ -87,28 +67,11 @@ function draw(ctx) {
 }
 
 draw(ctx);
-// ハロウィン仕様
-let images = [tombstoneimg, ghostimg];
-let loadedCount = 1;
-for (let i in images) {
-    images[i].addEventListener('load', function() {
-        if (loadedCount == images.length) {
-            let n = 0;
-            for (let j in images) {
-                for(let k = 0; k < 4; k++){
-                    ctx.drawImage(images[j], 0 + n*150, 0 + k*250 + n*20, 348 - n*161, 240 - n*40);
-                }
-                n++;
-            }
-        }
-        loadedCount++;
-    }, false);
-}
 
 canvas.addEventListener("click", (e) => {
     let x = e.clientX - canvas.getBoundingClientRect().left;
     let y = e.clientY - canvas.getBoundingClientRect().top;
-    if(x < /*200*/ 350){
+    if(x < 200){
         if(y < 240) {players[0] = -players[0];}
         if(y > 250 && y < 490) {players[1] = -players[1];}
         if(y > 500 && y < 740) {players[2] = -players[2];}
